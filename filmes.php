f76e9b7dea31e8c61376f10e92ae325d3539590b
jhenifer oliveira <42181112+jheniferoliveira@users.noreply.github.com>
2019-09-24 19:42:13 -0300

    Add files via upload

---
+++ filmes.php
@@ -0,0 +1,14 @@
+<?php
+
+$infoOIncrívelHulk  = array('Vingadores', '180m', '2019');
+$infoThor = array('Capitã marvel', '120m', '2019');
+$infoGuardioesDaGalaxia = array('Homem Formiga', '120m', '2019');
+
+list($Nome, $duracao, $ano) = $infoOIncrívelHulk ;
+echo "$Nome, $duracao e $ano. <br>";
+
+list($Nome, $duracao, $ano) = $infoThor;
+echo "$Nome, $duracao e $ano. <br>";
+
+list($Nome, $duracao, $ano) = $infoGuardioesDaGalaxia;
+echo "$Nome, $duracao e $ano. <br>";
